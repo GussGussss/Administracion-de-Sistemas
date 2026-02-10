09d35a0198934710ea448d95839e1de32702ded8
GussGussss <gustavopacheco3005@gmail.com>
2026-02-10 08:30:41 -0600

    Auto-sync Windows: 2026-02-10 08:30:41

--- Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
+++ Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
@@ -3,3 +3,19 @@ $hostname = hostname
 $ipActual = (get-netipaddress -addressfamily IPv4 | where-object { $_.interfacealias -notlike "*Loopback*" } | select-object -first 1).ipaddress
 write-host "Host: $hostname"
 write-host "IP: $ipActual"
+
+function validar-ip{
+	param ([string]$IP)
+	return $IP -match '^([0-9]{1,3}\.){3}[0-9]{1,3}$'
+}
+
+function pedir-ip{
+	param([string]$mensaje)
+	do {
+		$ip = read-host $mensaje
+		if (-not (validar-ip $ip)) {
+			write-host "Esta mal: la IP es invalida"
+		}
+	}while (-not (validar-ip $ip))
+	return $ip
+}
