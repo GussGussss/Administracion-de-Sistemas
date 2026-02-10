df317e83a2ce5fd0eb240acc9b42e34232d9b0e0
GussGussss <gustavopacheco3005@gmail.com>
2026-02-10 08:49:05 -0600

    Auto-sync Windows: 2026-02-10 08:49:04

--- Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
+++ Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
@@ -19,3 +19,65 @@ function pedir-ip{
 	}while (-not (validar-ip $ip))
 	return $ip
 }
+
+function intalar-dhcp{
+	$dhcp = get-windowsfeature DHCP
+	if (-not $dhcp.installed){
+		write-host "Instalando servicio DHCP"
+		install-windowsfeature DHCP-include-managementools
+	}else{
+		write-host "El servicio DHCP ya esta instalado :p"
+	}
+}
+
+function configurar-dhcp{
+	instalar-dhcp
+	write-host "***** CONFIGURACION DEL DHCP ******"
+	$ambito = read-host "Nombre del ambito: "
+	$segmento = pedir-ip "Ingrese el segmento de red (ej: 192.168.0.0): "
+	$prefijo = read-host "Prefijo (ej: 24): "
+	$rangoInicial = pedir-ip "Ingrese el rango inicial de la IP (ej: 192.168.0.100): "
+	$rangoFinal = pedir-ip "Ingrese el rango final de la IP (ej: 192.168.0.150): "
+	$gateway = pedir-ip "Ingrese el gateway (ej: 192.168.0.1): "
+	$dns = pedir-ip "Ingrese el DNS (ej: 192.168.0.71): "
+
+	write-host ""
+	write-host "**** Datos ingresados ****"
+	write-host "Segmento de red: $segmento"
+	write-host "Rango: $rangoInicial - $rangoFinal"
+	write-host "Gateway: $gateway"
+	write-host "DNS: $dns"
+
+	$segmentoServidor = (($ipActual -split '\.') [0..2] -join '.') + ".0"
+	
+	if ($segmento -ne $segmentoServidor) {
+		write-host "Esta mal: el segmento debe coincidir con el segmentod el servidor"
+		write-host "IP del servidor: $ipActual"
+		write-host "Segmento ingresado: $segmento"
+		exit 1
+	}
+	
+	$mask = switch ($prefijo){
+	24 {"255.255.255.0"}
+	16 {"255.255.0.0"}
+	default {"255.255.255.0"}
+	}
+
+	add-dhcpserverv4scope '
+		-Name $ambito '
+		-StartRange $rangoInicial '
+		-EndRange $rangoFinal '
+		-SubNetmask $mask '
+		-State Active
+	
+	set-dhcpserverv4optionvalue '
+		-Router $gateway
+		-DnsServer $dns
+
+	write-host "Servidor DHCP configurado aca chilo :p"
+}
+
+
+
+
+
