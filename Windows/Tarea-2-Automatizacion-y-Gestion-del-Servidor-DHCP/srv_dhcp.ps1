eb7c081ff406269993844d968e71d57b0001d3fd
GussGussss <gustavopacheco3005@gmail.com>
2026-02-10 09:04:03 -0600

    Auto-sync Windows: 2026-02-10 09:04:03

--- Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
+++ Windows/Tarea-2-Automatizacion-y-Gestion-del-Servidor-DHCP/srv_dhcp.ps1
@@ -1,4 +1,4 @@
-write-host "****** AUTOMATIZACION Y GESTION DEL SERVIDOR DHCP *****
+write-host "****** AUTOMATIZACION Y GESTION DEL SERVIDOR DHCP *****"
 $hostname = hostname
 $ipActual = (get-netipaddress -addressfamily IPv4 | where-object { $_.interfacealias -notlike "*Loopback*" } | select-object -first 1).ipaddress
 write-host "Host: $hostname"
