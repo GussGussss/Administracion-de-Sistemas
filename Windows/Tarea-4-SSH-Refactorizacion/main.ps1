1038e42d76ec837bd757a143277079da5d007f3d
GussGussss <gustavopacheco3005@gmail.com>
2026-02-24 09:17:03 -0700

    Update main.ps1

--- Windows/Tarea-4-SSH-Refactorizacion/main.ps1
+++ Windows/Tarea-4-SSH-Refactorizacion/main.ps1
@@ -2,3 +2,46 @@
 . .\modulos\dhcp.ps1
 . .\modulos\dns.ps1
 . .\modulos\ssh.ps1
+
+function menu-principal {
+
+    do {
+        Write-Host ""
+        Write-Host "******** Menu Principal ********"
+        Write-Host "1) DHCP"
+        Write-Host "2) DNS"
+        Write-Host "3) SSH"
+        Write-Host "4) Estado general de servicios"
+        Write-Host "0) Salir"
+
+        $opcion = Read-Host "Selecciona una opcion"
+
+        switch ($opcion) {
+
+            "1" { menu-dhcp }
+
+            "2" { menu-dns }
+
+            "3" { menu_ssh }
+
+            "4" {
+                Write-Host ""
+                Write-Host "***** Estado de servicios *****"
+                Get-Service DHCPServer
+                Get-Service DNS
+                Get-Service sshd -ErrorAction SilentlyContinue
+                Read-Host "Presiona ENTER para continuar"
+            }
+
+            "0" { break }
+
+            default {
+                Write-Host "Opcion invalida"
+                Read-Host "Presiona ENTER para continuar"
+            }
+        }
+
+    } while ($true)
+}
+
+menu-principal
