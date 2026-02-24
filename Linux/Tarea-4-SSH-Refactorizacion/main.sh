808a482e838656fe194bb0513d6a838f70780a4f
GussGussss <gustavopacheco3005@gmail.com>
2026-02-23 23:43:37 -0700

    Update main.sh

--- Linux/Tarea-4-SSH-Refactorizacion/main.sh
+++ Linux/Tarea-4-SSH-Refactorizacion/main.sh
@@ -3,6 +3,8 @@ source ./lib/network.sh
 source ./modulos/dhcp.sh
 source ./modulos/dns.sh
 source ./modulos/ssh.sh
+ipActual=$(ip -4 addr show enp0s8 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
+export ipActual
 
 echo "Pruebas"
 if validar_ip "192.168.0.1"; then
