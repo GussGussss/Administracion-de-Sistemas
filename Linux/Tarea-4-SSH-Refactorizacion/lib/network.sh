0afce4e5626c6aa46cbce12f5ff1f287cc7b9126
GussGussss <gustavopacheco3005@gmail.com>
2026-02-23 22:42:05 -0700

    Implement IP validation and input functions
    Add functions to validate IP address range and prompt for IP input.

--- Linux/Tarea-4-SSH-Refactorizacion/lib/network.sh
+++ Linux/Tarea-4-SSH-Refactorizacion/lib/network.sh
@@ -18,3 +18,36 @@ validar_ip(){
 
 	return 0
 }
+
+validar_ip_rango(){
+	local ip=$1
+	IFS='.' read -r o1 o2 o3 o4 <<< "$ip"
+	for octeto in $o1 $o2 $o3 $o4; do
+		if ((octeto < 0 || octeto > 255)); then
+			return 1
+		fi
+	done
+	return 0
+
+}
+
+pedir_ip(){
+	local mensaje=$1
+	local vacio=$2
+	local ip
+
+	while true; do
+		read -p "$mensaje: " ip
+		if [[ -z "$ip" && "$vacio" == "si" ]]; then
+			echo ""
+			return
+		fi
+
+		if validar_ip "$ip"; then
+			echo "$ip"
+			return
+		else
+			echo "Esta mal: IP invalida"
+		fi
+	done
+}
