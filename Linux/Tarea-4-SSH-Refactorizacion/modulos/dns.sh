c3ae464ff5bb9aa554866a2261a99f8f20b71f66
GussGussss <gustavopacheco3005@gmail.com>
2026-02-23 23:37:21 -0700

    Update dns.sh

--- Linux/Tarea-4-SSH-Refactorizacion/modulos/dns.sh
+++ Linux/Tarea-4-SSH-Refactorizacion/modulos/dns.sh
@@ -32,3 +32,60 @@ estado_dns(){
 	fi
 	read -p "Pesiona ENTER para continuar"
 }
+
+crear_dominio_principal(){
+	dominio="reprobados.com"
+	zona_file="/var/named/$dominio.db"
+
+	if sudo grep -q "$dominio" /etc/named.rfc1912.zones; then
+		echo "El dominio ya existe"
+		read -p "Presione ENTERN para continuar"
+		return
+	fi
+
+	read -p "Ingresa la IP del dominio (ej: 192.168.0.70 o presione ENTER para usar la IP del servidor) " ipDominio
+
+	if [[ -z "$ipDominio" ]]; then
+		ipDominio=$ipActual
+	fi
+
+	if ! validar_ip "$ipDominio"; then
+		echo "IP invalida"
+		read -p "Presione ENTER para continuar"
+		return
+	fi
+
+	echo "Creando dominio...."
+	sudo tee -a /etc/named.rfc1912.zones > /dev/null <<EOF
+
+zone "$dominio" IN {
+	type master;
+	file "$dominio.db";
+};
+EOF
+	echo "Creando archivo de zona...."
+	sudo tee $zona_file > /dev/null <<EOF
+\$TTL 86400
+@	IN	SOA	ns1.$dominio. admin.$dominio. (
+		2024021601
+		3600
+		1800
+		604800
+		86400 )
+
+@	IN 	NS	ns1.$dominio.
+ns1	IN	A	$ipActual
+@	IN	A	$ipDominio
+www	IN	CNAME	$dominio.
+EOF
+
+	sudo chown named:named $zona_file
+	sudo chmod 640 $zona_file
+
+	sudo named-checkconf
+	sudo named-checkzone $dominio $zona_file
+	sudo systemctl restart named
+	echo "Dominio configurado correctamente"
+	read -p "Presion ENTER para continuar"
+
+}
