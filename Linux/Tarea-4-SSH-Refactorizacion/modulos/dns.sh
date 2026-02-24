a2e4146e93fe9562f74d2e8406f01f8078c332c5
GussGussss <gustavopacheco3005@gmail.com>
2026-02-23 23:36:35 -0700

    Update dns.sh

--- Linux/Tarea-4-SSH-Refactorizacion/modulos/dns.sh
+++ Linux/Tarea-4-SSH-Refactorizacion/modulos/dns.sh
@@ -1,1 +1,23 @@
-echo "dns"
+#!/bin/bash
+instalar_dns(){
+	echo ""
+	echo "Verificando si DNS (BIND) esta instalado...."
+
+	if rpm -q bind &>/dev/null; then
+		echo "El servicio DNS (BIND) ya esta instalado"
+	else
+		echo "instalando DNS (BIND)...."
+		sudo dnf install -y bind bind-utils
+		echo "instalacion completada"
+	fi
+
+	sudo systemctl enable named
+	sudo systemctl start named
+	configurar_named_base
+
+	echo ""
+	echo "Configurando firewall para permitir DNS"
+	sudo firewall-cmd --permanent --add-service=dns
+	sudo firewall-cmd --reload
+	read -p "presiona ENTER para continuar"
+}
