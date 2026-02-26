2247d651df5345155eecfed722a386ae5a564eb6
GussGussss <gustavopacheco3005@gmail.com>
2026-02-26 00:45:35 -0700

    Update network.sh

--- Linux/Tarea-4-SSH-Refactorizacion/lib/network.sh
+++ Linux/Tarea-4-SSH-Refactorizacion/lib/network.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 validar_ip(){
-	local ip=$1
-	local expresionRegular="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
-	if ! [[ $ip =~ $expresionRegular ]]; then
-		return 1
-	fi
+    local ip=$1
+    local expresionRegular="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+    if ! [[ $ip =~ $expresionRegular ]]; then
+        return 1
+    fi
 
-	IFS='.' read -r o1 o2 o3 o4 <<< "$ip"
-	for octeto in $o1 $o2 $o3 $o4; do
-		if ((octeto < 0 || octeto > 255)); then
-			return 1
-		fi
-	done
+    IFS='.' read -r o1 o2 o3 o4 <<< "$ip"
+    for octeto in $o1 $o2 $o3 $o4; do
+        if ((octeto < 0 || octeto > 255)); then
+            return 1
+        fi
+    done
 
-	if [[ "$ip" == "0.0.0.0" || "$ip" == "255.255.255.255" || "$ip" == "127.0.0.1" || "$ip" == "127.0.0.0" ]]; then
-		return 1
-	fi
+    if [[ "$ip" == "0.0.0.0" || "$ip" == "255.255.255.255" || "$ip" == "127.0.0.1" || "$ip" == "127.0.0.0" ]]; then
+        return 1
+    fi
 
-	return 0
+    return 0
 }
 
 validar_ip_rango(){
