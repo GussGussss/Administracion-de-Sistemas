ee1a0e86265cb4e33277c85cf4b623e7ca0b45ba
GussGussss <gustavopacheco3005@gmail.com>
2026-03-01 18:59:38 -0700

    Update funcion instalar ftp.sh

--- Linux/Tarea-5-Automatizacion-de-Servidor-FTP/ftp.sh
+++ Linux/Tarea-5-Automatizacion-de-Servidor-FTP/ftp.sh
@@ -12,13 +12,33 @@ instalar_ftp(){
 
   if rpm -q vsftpd &>/dev/null; then
     echo "El servicio vsftpd ya esta instalado :D"
+    while true; do
+      read -p "Desea reinstalarlo (s/n)?: " opcion
+      echo ""
+      case $opcion in
+        s|S)
+          echo "Reinstalando el servicio vsftpd...."
+          dnf reinstall -y vsftpd > /dev/null 2>&1
+          echo ""
+          echo "Reinstalacion completada :D"
+          break ;;
+        n|N)
+          echo "No se realizara ninguna accion"
+          break ;;
+        *)
+          echo "Opcion invalida... ingrese s o n" ;;
+      esac
+    done
   else
     echo "El servicio vsftpd no esta instalado"
     echo ""
     echo "Instalado....."
-    dnf install -y vsftpd
-    echo "Instalacion completada :D"
-    echo ""
+    dnf install -y vsftpd > /dev/null 2>&1
+      if rpm -q vsftpd &>/dev/null; then
+  			echo "Instalación completada :D"
+  		else
+  			echo "Hubo un error en la instalación."
+  		fi
   fi
 
   if systemctl is-active --quiet vsftpd; then
@@ -28,6 +48,8 @@ instalar_ftp(){
     systemctl start vsftpd
     systemctl enable vsftpd
   fi
+
+  read -p "Presione ENTER para continuar..."
 }
 
 menu(){
