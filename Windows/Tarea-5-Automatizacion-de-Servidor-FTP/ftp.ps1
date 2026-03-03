6e805fa89ee2a083d4469f5e76f82aee98d82a56
GussGussss <gustavopacheco3005@gmail.com>
2026-03-03 04:13:55 -0700

    Update ftp.ps1

--- Windows/Tarea-5-Automatizacion-de-Servidor-FTP/ftp.ps1
+++ Windows/Tarea-5-Automatizacion-de-Servidor-FTP/ftp.ps1
@@ -31,3 +31,71 @@ function Configurar-Firewall {
     Write-Host "Firewall configurado correctamente para FTP."
 }
 
+Write-Host "****** Tarea 5: Automatizacion de Servidor FTP ********"
+
+function Instalar-FTP {
+
+    Write-Host ""
+    Write-Host "Verificando si el servicio FTP (IIS) esta instalado..."
+    Write-Host ""
+
+    $ftpFeature = Get-WindowsFeature -Name Web-Ftp-Server
+
+    if ($ftpFeature.Installed) {
+
+        Write-Host "El servicio FTP ya esta instalado :D"
+
+        while ($true) {
+            $opcion = Read-Host "Desea reinstalarlo (s/n)?"
+            Write-Host ""
+
+            switch ($opcion.ToLower()) {
+                "s" {
+                    Write-Host "Reinstalando el servicio FTP..."
+
+                    Remove-WindowsFeature -Name Web-Ftp-Server -ErrorAction SilentlyContinue
+                    Install-WindowsFeature -Name Web-Server,Web-Ftp-Server -IncludeManagementTools
+
+                    Write-Host ""
+                    Write-Host "Reinstalacion completada :D"
+                    break
+                }
+                "n" {
+                    Write-Host "No se realizara ninguna accion"
+                    break
+                }
+                default {
+                    Write-Host "Opcion invalida... ingrese s o n"
+                }
+            }
+        }
+
+    } else {
+
+        Write-Host "El servicio FTP no esta instalado"
+        Write-Host ""
+        Write-Host "Instalando..."
+
+        $resultado = Install-WindowsFeature -Name Web-Server,Web-Ftp-Server -IncludeManagementTools
+
+        if ($resultado.Success) {
+            Write-Host "Instalacion completada :D"
+        } else {
+            Write-Host "Hubo un error en la instalacion."
+        }
+    }
+
+    if ((Get-Service FTPSVC).StartType -ne "Automatic") {
+        Write-Host "Habilitando servicio..."
+        Set-Service -Name FTPSVC -StartupType Automatic
+    }
+
+    if ((Get-Service FTPSVC).Status -ne "Running") {
+        Write-Host "Iniciando servicio..."
+        Start-Service FTPSVC
+    }
+
+    Configurar-Firewall
+
+    Read-Host "Presione ENTER para continuar..."
+}
