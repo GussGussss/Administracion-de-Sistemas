97db451b040f9fbad3f55cee640849ff2e8a30d7
GussGussss <gustavopacheco3005@gmail.com>
2026-03-09 18:59:52 -0700

    Update main.sh

--- Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/main.sh
+++ Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/main.sh
@@ -18,8 +18,30 @@ while true
     
     case $OPCION in
         
-        1)
-        echo "Seleccionaste Apache"
+       1)
+        listar_versiones_apache
+        
+        read -p "Seleccione número de versión: " VERSION_NUM
+        
+        VERSION=$(dnf list --showduplicates httpd \
+        | grep httpd.x86_64 \
+        | awk '{print $2}' \
+        | sed -n "${VERSION_NUM}p")
+        
+        read -p "Ingrese puerto: " PUERTO
+        
+        validar_puerto $PUERTO
+        
+        if [ $? -eq 0 ]; then
+        
+        instalar_apache $VERSION $PUERTO
+        
+        else
+        
+        echo "Puerto inválido"
+        
+        fi
+        
         ;;
         
         2)
