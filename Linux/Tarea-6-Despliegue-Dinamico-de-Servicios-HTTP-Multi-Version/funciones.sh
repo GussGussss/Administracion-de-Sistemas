7fec4a0c6d9ee1f899d301dc58006709bebcc90b
GussGussss <gustavopacheco3005@gmail.com>
2026-03-10 23:11:08 -0700

    Update funciones.sh

--- Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/funciones.sh
+++ Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/funciones.sh
@@ -390,7 +390,9 @@ permitir_puerto_selinux $PUERTO
 crear_index "Tomcat" "$VERSION" "$PUERTO" "/opt/tomcat/webapps/ROOT"
 
 # iniciar tomcat
-sudo -u tomcatsvc env JAVA_HOME=/usr/lib/jvm/java-17-openjdk CATALINA_HOME=/opt/tomcat /opt/tomcat/bin/startup.sh
+JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
+
+sudo -u tomcatsvc env JAVA_HOME=$JAVA_HOME CATALINA_HOME=/opt/tomcat /opt/tomcat/bin/startup.sh
 
 echo "Esperando a que Tomcat inicie..."
 
