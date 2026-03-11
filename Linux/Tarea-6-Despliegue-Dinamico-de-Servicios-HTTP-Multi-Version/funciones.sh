ca275f153ec77e43f0bac0fd52a093f28ca9feb8
GussGussss <gustavopacheco3005@gmail.com>
2026-03-11 05:04:32 -0700

    Update funciones.sh

--- Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/funciones.sh
+++ Linux/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/funciones.sh
@@ -211,25 +211,24 @@ systemctl restart httpd > /dev/null 2>&1
 #########################################
 
 listar_versiones_nginx() {
-    echo "Buscando versiones de Nginx disponibles..."
 
-    VERSIONES=$(dnf repoquery --showduplicates nginx | awk -F'-' '{print $2}' | sort -V | uniq)
+echo "Versiones disponibles de Nginx:"
+echo ""
 
-    COUNT=$(echo "$VERSIONES" | wc -l)
+VERSIONES=$(dnf list --showduplicates nginx \
+| grep nginx.x86_64 \
+| awk '{print $2}' \
+| sort -V \
+| uniq)
 
-    if [ "$COUNT" -lt 3 ]; then
-        LATEST="1.24.0"
-        LTS="1.22.1"
-        OLDEST="1.20.1"
-    else
-        OLDEST=$(echo "$VERSIONES" | head -n 1)
-        LATEST=$(echo "$VERSIONES" | tail -n 1)
-        LTS=$(echo "$VERSIONES" | sed -n '2p')
-    fi
+OLDEST=$(echo "$VERSIONES" | head -n 1)
+LATEST=$(echo "$VERSIONES" | tail -n 1)
+LTS=$(echo "$VERSIONES" | sed -n '2p')
 
-    echo "1) $LATEST  (Latest / Desarrollo)"
-    echo "2) $LTS     (LTS / Estable)"
-    echo "3) $OLDEST  (Oldest)"
+echo "1) $LATEST  (Latest / Desarrollo)"
+echo "2) $LTS     (LTS / Estable)"
+echo "3) $OLDEST  (Oldest)"
+
 }
 
 #########################################
@@ -305,7 +304,7 @@ gestionar_puerto $PUERTO || return 1
 
 echo "Instalando Nginx versión $VERSION..."
 
-dnf install -y nginx-$VERSION --allowerasing > /dev/null 2>&1
+dnf install -y nginx-$VERSION > /dev/null 2>&1
 
 crear_usuario_nginx
 
