90a84707975de4aeff5423eb5fdc2f63172c816c
GussGussss <gustavopacheco3005@gmail.com>
2026-03-11 15:10:41 -0700

    Update main.ps1

--- Windows/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/main.ps1
+++ Windows/Tarea-6-Despliegue-Dinamico-de-Servicios-HTTP-Multi-Version/main.ps1
@@ -1,1 +1,78 @@
-write-host "main.ps1"
+. .\funciones.ps1
+
+while ($true) {
+
+Write-Host "=============================="
+Write-Host " DESPLIEGUE SERVIDORES HTTP "
+Write-Host "=============================="
+
+Write-Host "1) IIS"
+Write-Host "2) Apache"
+Write-Host "3) Nginx"
+Write-Host "4) Salir"
+
+$opcion = Read-Host "Seleccione una opción"
+
+switch ($opcion) {
+
+"1" {
+
+listar_versiones_iis
+
+$puerto = Read-Host "Ingrese puerto"
+
+if (validar_puerto $puerto) {
+
+instalar_iis $puerto
+
+}
+
+}
+
+"2" {
+
+listar_versiones_apache_win
+
+$version = Read-Host "Seleccione versión"
+
+$puerto = Read-Host "Ingrese puerto"
+
+if (validar_puerto $puerto) {
+
+instalar_apache_win $version $puerto
+
+}
+
+}
+
+"3" {
+
+listar_versiones_nginx_win
+
+$version = Read-Host "Seleccione versión"
+
+$puerto = Read-Host "Ingrese puerto"
+
+if (validar_puerto $puerto) {
+
+instalar_nginx_win $version $puerto
+
+}
+
+}
+
+"4" {
+
+exit
+
+}
+
+default {
+
+Write-Host "Opción inválida"
+
+}
+
+}
+
+}
