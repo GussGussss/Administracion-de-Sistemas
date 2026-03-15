3fe9fd68814dd2f6bcc027892b489ad49e6ca44d
GussGussss <gustavopacheco3005@gmail.com>
2026-03-15 00:16:46 -0700

    Auto-sync Windows: 2026-03-15 00:16:46

---
+++ sync_auto.ps1
@@ -0,0 +1,10 @@
+﻿# Forzar la ruta de Git en la sesión actual
+$env:Path += ";C:\Program Files\Git\bin;C:\Program Files\Git\cmd"
+
+Set-Location "C:\Users\Administrator\Administracion-de-Sistemas"
+
+git pull origin main
+git add .
+$fecha = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+git commit -m "Auto-sync Windows: $fecha"
+git push origin main
