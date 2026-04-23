456c79e5495fb163800a8c2f8912c2070bd912e2
GussGussss <gustavopacheco3005@gmail.com>
2026-04-22 22:21:16 -0700

    Update funciones_p8.ps1

--- Windows/pruebas/practica-8/funciones_p8.ps1
+++ Windows/pruebas/practica-8/funciones_p8.ps1
@@ -5,7 +5,7 @@
 #  Version  : Final (hash hardcodeado - cliente Windows 10)
 #
 #  Hash de notepad.exe del cliente Windows 10 Pro:
-#  0x0C386FA6ABFDEFFBBEFF5BCE97D461340A23D1981458607BD9E5EEFF4066789A
+#  0xDA5807BB0997CC6B5132950EC87EDA2B33B1AC4533CF1F7A22A6F3B576ED7C5
 #  Tamano: 201216 bytes
 # ============================================================
 
@@ -817,7 +817,7 @@ function Configurar-Apantallamiento {
 # FUNCION 7: Configurar AppLocker (hash hardcodeado)
 #
 # Hash conocido de notepad.exe del cliente Windows 10 Pro:
-# 0x0C386FA6ABFDEFFBBEFF5BCE97D461340A23D1981458607BD9E5EEFF4066789A
+# 0xDA5807BB0997CC6B5132950EC87EDA2B33B1AC4533CF1F7A22A6F3B576ED7C5
 # Tamano: 201216 bytes
 #
 # Reglas:
@@ -846,7 +846,7 @@ function Configurar-AppLocker {
     Write-Host "    NoCuates : notepad.exe BLOQUEADO por Hash" -ForegroundColor Cyan
     Write-Host ""
     Write-Host "  Hash usado (notepad.exe Windows 10 Pro):" -ForegroundColor White
-    Write-Host "  0x70152C176B629E51FD283BD2F30ACFBDB1A129EA14D94889C1D32A742C104BBF..." -ForegroundColor DarkGray
+    Write-Host "  0xDA5807BB0997CC6B5132950EC87EDA2B33B1AC4533CF1F7A22A6F3B576ED7C5..." -ForegroundColor DarkGray
     Write-Host ""
     Write-Host "  La regla de Hash identifica el archivo por su" -ForegroundColor Yellow
     Write-Host "  contenido, no por su nombre. Renombrar el .exe" -ForegroundColor Yellow
@@ -874,7 +874,7 @@ function Configurar-AppLocker {
     }
 
     # --- Hash hardcodeado del cliente Windows 10 Pro ---
-    $hashValor   = "0x70152C176B629E51FD283BD2F30ACFBDB1A129EA14D94889C1D32A742C104BBF"
+    $hashValor   = "0xDA5807BB0997CC6B5132950EC87EDA2B33B1AC4533CF1F7A22A6F3B576ED7C5"
     $archivoSize = 200704
 
     # --- Construir XML ---
