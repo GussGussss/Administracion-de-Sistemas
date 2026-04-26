d474829671c0464d848e9ab85461e988936e9f9c
GussGussss <gustavopacheco3005@gmail.com>
2026-04-26 01:49:03 -0700

    Auto-sync Linux: 2026-04-26 01:49:03

---
+++ sync_auto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "/home/srv-linux-sistemas/Administracion-de-Sistemas"
+
+git pull origin main
+git add .
+fecha=$(date + "%Y-%m-%d %H:%M:%S")
+
+git commit -m "Auto-sync Linux: $fecha"
+git push origin main
