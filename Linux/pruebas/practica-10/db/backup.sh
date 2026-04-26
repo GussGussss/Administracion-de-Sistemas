cc9a1b9462a8b315551d6e767c154bbae32e7abd
GussGussss <gustavopacheco3005@gmail.com>
2026-04-26 15:02:21 -0700

    Delete Linux/pruebas/practica-10/db directory

--- Linux/pruebas/practica-10/db/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script de respaldo generado automaticamente
-fecha=$(date +"%Y%m%d_%H%M%S")
-docker exec db_postgres pg_dump -U admin_user practica_db > ./db/respaldo_$fecha.sql
-echo "Respaldo creado: respaldo_$fecha.sql"
