df87c233902e9116b5b9512372aa0429e567c4bf
GussGussss <gustavopacheco3005@gmail.com>
2026-04-26 12:33:53 -0700

    refactor: reordenar menú interactivo para usar opción 0 como salida estándar

--- Linux/pruebas/practica-10/main_p10.sh
+++ Linux/pruebas/practica-10/main_p10.sh
@@ -4,15 +4,17 @@
 source ./funciones_p10.sh
 
 mostrar_menu() {
+    clear
     echo "=========================================="
     echo "   ADMINISTRACIÓN DE SISTEMAS - TAREA 10"
     echo "=========================================="
-    echo "1. Preparar Entorno (Carpetas, Red, Volúmenes)"
-    echo "2. Desplegar Servidor Web (Nginx + Seguridad)"
-    echo "3. Desplegar Base de Datos (PostgreSQL)"
-    echo "4. Desplegar Servidor FTP"
-    echo "5. Limpiar Contenedores (Reset)"
-    echo "6. Salir"
+    echo "1. Instalar Dependencias (Docker Engine)"
+    echo "2. Preparar Entorno (Carpetas, Red, Volúmenes)"
+    echo "3. Desplegar Servidor Web (Nginx + Seguridad)"
+    echo "4. Desplegar Base de Datos (PostgreSQL)"
+    echo "5. Desplegar Servidor FTP"
+    echo "6. Limpiar Contenedores (Reset)"
+    echo "0. Salir"
     echo "=========================================="
     echo -n "Seleccione una opción: "
 }
@@ -21,12 +23,13 @@ while true; do
     mostrar_menu
     read opcion
     case $opcion in
-        1) preparar_entorno ;;
-        2) echo "Próximamente: Dockerfile Web..." ;;
-        3) echo "Próximamente: Config BD..." ;;
-        4) echo "Próximamente: Config FTP..." ;;
-        5) limpiar_todo ;;
-        6) exit 0 ;;
+        1) instalar_docker ;;
+        2) preparar_entorno ;;
+        3) echo "Próximamente: Dockerfile Web..." ;;
+        4) echo "Próximamente: Config BD..." ;;
+        5) echo "Próximamente: Config FTP..." ;;
+        6) limpiar_todo ;;
+        0) echo "Saliendo del sistema..."; exit 0 ;;
         *) echo "Opción no válida." ;;
     esac
     echo "Presione enter para continuar..."
